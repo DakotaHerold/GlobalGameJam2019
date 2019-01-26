c22b8522e4cb33c8a469c33ec59b8d712c9a9bef
Ed Amidon <bengals2354@gmail.com>
2019-01-26 18:12:59 -0500

    updates to player and flashlight

--- GlobalGameJam2019/Assets/Scripts/Actors/Player.cs
+++ GlobalGameJam2019/Assets/Scripts/Actors/Player.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // enums for states
+    private enum PlayerAnimState
+    {
+        standing = 0,
+        walking = 1,
+        hit = 2
+    }
+
     // attributes
     private float velocityX, velocityY;
+    private float mouseX, mouseY;
     private Camera cam;
     private Vector2 mouseLocation;
     private Vector3 direction;
+    private BoxCollider2D collisionBox;
+    private bool flashlightOn;
 
+    // flashlight attributes
+    private float viewRadius;
+    private float viewAngle;
+
+    [SerializeField]
+    private PlayerAnimState animState;
     [SerializeField]
     private int health;
     [SerializeField]
-    private float mouseX, mouseY;
-    [SerializeField]
     private float speed = 2.0f;
-
     private float rotationSpeed = 4.0f;
-
-    private BoxCollider2D collisionBox;
+    private float flashDistance = 2.5f;
 
     // Awake is called before first frame update
     void Awake()
     {
+        flashlightOn = false;
         cam = Camera.main;
         health = 3;
         collisionBox = GetComponent<BoxCollider2D>();
@@ -35,7 +49,6 @@ public class Player : MonoBehaviour
     {
         // get mouse position
         mouseLocation = cam.ScreenToWorldPoint(InputHandler.Instance.MousePos);
-        //direction = mouseLocation - transform.position;
 
         // handle movement input
         velocityX += InputHandler.Instance.HorizontalAxis * speed;
@@ -46,15 +59,23 @@ public class Player : MonoBehaviour
 
         transform.Translate(velocityX, velocityY, 0, Space.World);
 
-        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg));
-
-        //**** slow rotation down for polish
+        //**** slow rotation down for polish?
 
         // get direction you want to point at
         Vector2 direction = (mouseLocation - (Vector2)transform.position).normalized;
 
         // set vector of transform directly
         transform.up = direction;
+
+        // state handling
+        if (velocityX != 0 || velocityY != 0)
+        {
+            animState = PlayerAnimState.walking;
+        }
+        else
+        {
+            animState = PlayerAnimState.standing;
+        }
 
     }
 }
