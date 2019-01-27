f5294195cfdad3df8e106210f4db959b0d90d533
Ed Amidon <bengals2354@gmail.com>
2019-01-27 11:53:47 -0500

    UI updates, starting work on game flow

--- GlobalGameJam2019/Assets/Game Jam Template/Scripts/Menu/ShowPanels.cs
+++ GlobalGameJam2019/Assets/Game Jam Template/Scripts/Menu/ShowPanels.cs
@@ -8,6 +8,7 @@ public class ShowPanels : MonoBehaviour {
 	public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
 	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
 	public GameObject pausePanel;                           //Store a reference to the Game Object PausePanel 
+    public GameObject creditsPanel;                           //Store a reference to the Game Object CreditsPanel 
 
     private GameObject activePanel;                         
     private MenuObject activePanelMenuObject;
@@ -78,4 +79,22 @@ public class ShowPanels : MonoBehaviour {
 		optionsTint.SetActive(false);
 
 	}
+
+    //Call this function to activate and display the Pause panel during game play
+    public void ShowCreditsPanel()
+    {
+        creditsPanel.SetActive(true);
+        optionsTint.SetActive(true);
+        menuPanel.SetActive(false);
+        SetSelection(creditsPanel);
+
+    }
+
+    //Call this function to deactivate and hide the Options panel during the main menu
+    public void HideCreditsPanel()
+    {
+        menuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        optionsTint.SetActive(false);
+    }
 }
